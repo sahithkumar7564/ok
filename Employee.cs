3e052be98c19b3e8256dfdd6ebcf33d30fec15ab
sahith0293 <sahithpinku37784@gmail.com>
2022-04-03 22:19:32 +0530

    de

---
+++ Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EmployeeMaintanenceBL
+{
+    public class Employee
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int age { get; set; }
+
+        public int EmployeeID { get; set; }
+         
+        public Employee(int EmployeeID,int age,string FirstName,string LastName,)
+        {
+            this.EmployeeID = EmployeeID;
+            this.age = age;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+
+
+
+
+    }
+
+}
---
+++ EmployeeDAL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeMaintanenceBL
+{
+    internal class EmployeeDAL
+    {
+       
+        public List<Employee> GetEmployeeData( )
+        {
+            ;
+            ContractEmployee cobj1 = new ContractEmployee();
+            PermanentEmployee cobj2 = new PermanentEmployee();
+
+
+            employees.Add(cobj1);
+            employees.Add(cobj2);
+            return employees;
+
+            
+          
+        }
+    }
+}
